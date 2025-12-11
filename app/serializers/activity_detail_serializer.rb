01fe42802fe00acf7d7baf315d8626032eecc98d
Tadas Maksimavicius <tadasmak@gmail.com>
2025-12-11 19:43:44 +0200

    Pass activity_type data in Activity serializers

--- app/serializers/activity_detail_serializer.rb
+++ app/serializers/activity_detail_serializer.rb
@@ -1,5 +1,5 @@
 class ActivityDetailSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :location, :start_time, :max_participants, :minimum_age, :maximum_age, :created_at, :creator, :participants_count, :status
+  attributes :id, :title, :description, :location, :start_time, :max_participants, :minimum_age, :maximum_age, :created_at, :creator, :participants_count, :status, :activity_type
   belongs_to :creator, serializer: UserSerializer
   has_many :participants, serializer: UserSerializer
 end
