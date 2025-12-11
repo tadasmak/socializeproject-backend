a4e53f9a17ddcd73d7a4f9295b6107de74021ac3
Tadas Maksimavicius <tadasmak@gmail.com>
2025-12-11 21:04:13 +0200

    Fix Activity serializers

--- app/serializers/activity_detail_serializer.rb
+++ app/serializers/activity_detail_serializer.rb
@@ -1,5 +1,9 @@
 class ActivityDetailSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :location, :start_time, :max_participants, :minimum_age, :maximum_age, :created_at, :creator, :participants_count, :status, :activity_type
+  attributes :id, :title, :description, :location, :start_time, :max_participants, :minimum_age, :maximum_age, :created_at, :creator, :participants_count, :status, :activity_type_name
   belongs_to :creator, serializer: UserSerializer
   has_many :participants, serializer: UserSerializer
+
+  def activity_type_name
+    object.activity_type&.name
+  end
 end
